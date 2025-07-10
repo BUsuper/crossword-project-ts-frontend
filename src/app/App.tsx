d9fc1891765229003e02edaba687fbc3471e6b00
BUsuper <merzlyakov96@gmail.com>
2025-07-10 18:45:11 +1000

    Update App for use with TypeScript

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAppSelector } from "../store/hooks";
+import type { JSX } from "react";
+import {
+  CrosswordField,
+  EndgameModal,
+  Header,
+  QuestionsSection,
+} from "../components";
+import "./App.css";
+import { selectIsEndgameModalOpen } from "../slices/statusesSelectors";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(): JSX.Element {
+  const isEndgameModalOpen: boolean = useAppSelector(selectIsEndgameModalOpen);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div id="crosswordWrapprer">
+        <CrosswordField />
+        <QuestionsSection />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isEndgameModalOpen && <EndgameModal />}
     </>
-  )
+  );
 }
-
-export default App
