f916185719b1984783bd598c80c4c87b668cdd58
BUsuper <merzlyakov96@gmail.com>
2025-07-23 20:42:30 +1000

    Update CrosswordSelection to use REST API instead of importing crosswords from a file

--- src/components/CrosswordSelection/CrosswordSelection.tsx
+++ src/components/CrosswordSelection/CrosswordSelection.tsx
@@ -1,16 +1,21 @@
 import "./CrosswordSelection.scss";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import crosswords from "../../assets/crosswords";
 import {
   setCrossword,
   setHorizontalIterationOrder,
   setVerticalIterationOrder,
 } from "../../slices/crosswordSlice";
 import { selectIsFinished } from "../../slices/statusesSelectors";
-import type { FormEvent, JSX } from "react";
+import { useEffect, useState } from "react";
+import { type FormEvent, type JSX } from "react";
+import { loadCrossword, loadCrosswordKeys } from "../../utils/utils";
+import type { CrosswordFieldType } from "../../assets/crosswords";
 
 export function CrosswordSelection(): JSX.Element {
-  const crosswordNames: string[] = Object.keys(crosswords);
+  const [crosswordNames, setCrosswordNames] = useState<string[]>([]);
+  const [selectedCrosswordName, setSelectedCrosswordName] =
+    useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
   const isFinished: boolean = useAppSelector(selectIsFinished);
 
   const dispatch = useAppDispatch();
@@ -19,16 +24,50 @@ export function CrosswordSelection(): JSX.Element {
     e.preventDefault();
 
     // target is the whole form, crosswords is the name of select in it
-    const selectedCrossword = (
+    const selectedCrosswordName = (
       (e.target as HTMLFormElement).elements.namedItem(
         "crosswords"
       ) as HTMLSelectElement
     ).value;
 
-    dispatch(setCrossword(selectedCrossword));
-    dispatch(setVerticalIterationOrder(selectedCrossword));
-    dispatch(setHorizontalIterationOrder(selectedCrossword));
+    setSelectedCrosswordName(selectedCrosswordName);
   };
+
+  useEffect(() => {
+    const fetchKeys = async () => {
+      const keys: string[] | null = await loadCrosswordKeys();
+
+      if (keys === null) {
+        console.error("Cannot load the crossword keys");
+      } else {
+        setCrosswordNames(keys);
+      }
+
+      setLoading(false);
+    };
+
+    fetchKeys();
+  }, []);
+
+  useEffect(() => {
+    const fetchCrossword = async () => {
+      const crossword: CrosswordFieldType | null = await loadCrossword(
+        selectedCrosswordName
+      );
+
+      if (crossword === null) {
+        console.error("Cannot load the crossword");
+      } else {
+        dispatch(setCrossword(crossword));
+        dispatch(setVerticalIterationOrder(crossword));
+        dispatch(setHorizontalIterationOrder(crossword));
+      }
+    };
+
+    fetchCrossword();
+  }, [selectedCrosswordName, dispatch]);
+
+  console.log(crosswordNames);
 
   return (
     <form
@@ -37,11 +76,17 @@ export function CrosswordSelection(): JSX.Element {
       onSubmit={handleSubmit}
     >
       <select name="crosswords" id="CrosswordsSelection" disabled={isFinished}>
-        {crosswordNames.map((name) => (
-          <option value={name} key={name}>
-            {name}
-          </option>
-        ))}
+        {loading ? (
+          <option disabled>Loading</option>
+        ) : crosswordNames ? (
+          crosswordNames.map((name) => (
+            <option value={name} key={name}>
+              {name}
+            </option>
+          ))
+        ) : (
+          <option disabled>Error while loading crossword names</option>
+        )}
       </select>
       <input type="submit" value="Select" disabled={isFinished} />
     </form>
